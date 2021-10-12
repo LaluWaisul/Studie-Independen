dcd99716afe9ba0026adf7ab1881e18a95e5aaee
LaluWaisul <92082810+LaluWaisul@users.noreply.github.com>
2021-10-12 18:05:37 +0800

    Add files via upload

---
+++ java-code-challenge-1/src/com/nongsa/Main.java
@@ -0,0 +1,31 @@
+package com.nongsa;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String firstName;
+        String lastName;
+        Short age;
+        String alamat;
+        String phone;
+        String place;
+        String birth;
+
+        firstName = "Lalu Muhamad ";
+        lastName = "Waisul Kuroni";
+        age = 21;
+        alamat = "Lombok, NTB";
+        phone = "081937947739";
+        place = "Praya, Lombok Tengah, Nusa Tenggara Barat";
+        birth = "24th January 2000";
+
+
+        System.out.println("First Name      : "+firstName);
+        System.out.println("Last Name       : "+lastName);
+        System.out.println("Age             : "+age);
+        System.out.println("Address         : "+alamat);
+        System.out.println("Phone Number    : "+phone);
+        System.out.println("Place of Birth  : "+place);
+        System.out.println("Date of Birth   : "+birth);
+    }
+}
